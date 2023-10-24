999833dfd5b337142efe738975834cfda6609bee
yoshi <128337477+NBphamngocha@users.noreply.github.com>
2023-10-24 18:34:12 +0900

    PRのコメント対応

--- booklog/src/App.tsx
+++ booklog/src/App.tsx
@@ -1,30 +1,37 @@
 import { useState } from 'react'
 import { BookItem } from './types'
 import { MOCK_DATA } from './mockdata'
-import styles from './index.module.css'
+import styles from './App.module.css'
 
 
 const App = () => {
-  const [bookData, setBookData] = useState(MOCK_DATA.items)
-  const [book, setBook] = useState<BookItem>(bookData[0])
-  const { id, volumeInfo } = book;
-  const { title, authors, description, publisher, imageLinks, previewLink } = volumeInfo
-  const handleBookLink = () => {
-    window.open(`${previewLink}`, '_blank');
-  }
+  const [bookData, setBookData] = useState<BookItem[]>(MOCK_DATA.items)
+  const { title, authors, description, publisher, imageLinks, previewLink } = bookData[0].volumeInfo
+  console.log(typeof imageLinks)
 
   return (
     <>
       <main className={styles.boxWrap}>
-        <picture className={styles.boxPicture}>
-          <img src={imageLinks?.thumbnail} alt="React.js&Next.js超入門第2版" />
-        </picture>
+        {
+          imageLinks && (
+            <picture className={styles.boxPicture}>
+              <img src={imageLinks.thumbnail} alt="React.js&Next.js超入門第2版" />
+            </picture>
+          )
+        }
         <div className={styles.boxInfo}>
           <h2 className={styles.boxTitle}>{title}</h2>
-          <p className={styles.boxDescription}>{description}</p>
-          <p>著者:<span>{authors}</span></p>
-          <p>出版社:<span>{publisher}</span></p>
-          <button className={styles.boxButton} onClick={handleBookLink}>詳しく見る</button>
+          {
+            description && <p className={styles.boxDescription}>{description}</p>}
+          {
+            authors && <p>著者: <span>{authors.join("、")}</span></p>
+          }
+          {
+            publisher && <p>出版社: <span>{publisher}</span></p>
+          }
+          {
+            previewLink && <button className={styles.boxButton}><a href={previewLink} target="_blank">詳しく見る</a></button>
+          }
         </div>
       </main >
     </>
